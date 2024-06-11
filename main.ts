6c0c9fa1f589f8f5f79cf7c1b426176fca9a9a58
Ambreen Riaz <159724062+Ambreenriaz13@users.noreply.github.com>
2024-06-12 01:06:22 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+// creating a Array
+let pizza =  ["Chicken Tikka","Malai Cheez" , "Fajita"] 
+
+    // Using For- loop
+    for (let onepizza of pizza){
+        console.log(`I like ${ onepizza}pizza`);
+    }
+    
+// print a message outside of the for loop
+console.log("Piazz is love")
+console.log("I eat Piazz")
